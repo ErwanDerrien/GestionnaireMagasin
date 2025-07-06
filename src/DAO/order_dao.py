efc6aded2d83ef85d7fc3f08b33c9338a331ed9a
Erwan Derrien <erwanderrien@Erwans-MacBook-Pro.local>
2025-07-06 16:51:22 -0400

    Remove old capitalized folders

--- src/DAO/order_dao.py
+++
@@ -1,5 +0,0 @@
-# src/dao/order_dao.py
-from data.database import session
-from src.models.order import Order
-def query(order: Order): 
-    return session.query(order)
--- src/DAO/product_dao.py
+++
@@ -1,5 +0,0 @@
-# src/dao/product_dao.py
-from data.database import session
-from src.models.product import Product
-def query(product: Product): 
-    return session.query(product)
